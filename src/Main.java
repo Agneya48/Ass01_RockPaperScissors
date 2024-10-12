8cac73e8b78e05829610d9ebc1c526d23b36e33b
Agneya48 <josh.t.hampton@gmail.com>
2024-10-12 18:29:55 -0400

    Implemented Player 1 do while input loop

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,37 @@ public class Main {
         int playerAWins = 0;
         int playerBWins = 0;
         int ties = 0;
-        boolean done = false;
+        int gameCounter = 0;
+        String[] validInput = {"R", "P", "S"};
+        boolean okInput = false;
         String continueYN = "";
-        String trash = "";
 
         try (Scanner in = new Scanner(System.in)) { //close Scanner after program concludes to avoid memory leak
             System.out.println("Welcome to Rock, Paper, Scissors");
-            System.out.println("R or r for Rock, P or p for Paper, S or s for Scissors");
+            System.out.println("R/r - Rock  P/p - Paper  S/s - Scissors");
 
             do { // game will do at least 1 interation, will continue as long as y or Y is entered at prompt
 
-                done = false;
+                okInput = false;
+                gameCounter++;
+                System.out.println("\nGame " + gameCounter);
                 do { //input loop for player1
-                    System.out.print("\nPlayer 1, enter your move [RPS]: ");
-                    done = true; //placeholder until rest of program is written
+                    System.out.print("Player 1, enter your move. [RPS]: ");
+                    playerAMove = in.nextLine();
+                    for (String choice : validInput) {
+                        if (playerAMove.equalsIgnoreCase(choice)) {
+                            okInput = true;
+                            break;
+                        }
+                    }
+                    if (!okInput) {// no valid input entered
+                        System.out.println("\nInvalid move. Please choose between [RPS]");
+                    }
 
 
-                } while(!done);
+                } while(!okInput);
 
-                System.out.print("\nContinue? Y/N: ");
+                System.out.print("\nContinue? [Y/N]: ");
                 continueYN = in.nextLine();
 
             } while(continueYN.equalsIgnoreCase("Y"));
