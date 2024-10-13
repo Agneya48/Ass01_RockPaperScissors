047adf7f0e762ae948a5aee677104ddb11d4236c
Agneya48 <josh.t.hampton@gmail.com>
2024-10-12 20:07:12 -0400

    Completed Program

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,9 @@ public class Main {
                 okInput = false;
                 gameCounter++;
                 System.out.println("\nGame " + gameCounter);
-                do { // player1 input loop
-                    System.out.print("Player 1, enter your move. [RPS]: ");
+                System.out.println();
+                do { // playerA input loop
+                    System.out.print("Player A, enter your move. [RPS]: ");
                     playerAMove = in.nextLine();
                     for (String choice : validInput) {
                         if (playerAMove.equalsIgnoreCase(choice)) {
@@ -32,12 +33,12 @@ public class Main {
                         }
                     }
                     if (!okInput) { // no valid input entered
-                        System.out.println("\nInvalid move. Please choose between [RPS]");
+                        System.out.println("Invalid move. Please choose between R for Rock, P for Paper, or S for Scissors");
                     }
                 } while(!okInput); //loop until valid choice entered
                 okInput = false;
                 do { // player2 input loop
-                    System.out.print("Player 2, enter your move. [RPS]: ");
+                    System.out.print("Player B, enter your move. [RPS]: ");
                     playerBMove = in.nextLine();
                     for (String choice : validInput) {
                         if (playerBMove.equalsIgnoreCase(choice)) {
@@ -46,9 +47,75 @@ public class Main {
                         }
                     }
                     if (!okInput) { //no valid input entered
-                        System.out.println("\nInvalid move. Please choose between [RPS]");
+                        System.out.println("Invalid move. Please choose between R for Rock, P for Paper, or S for Scissors");
                     }
                 } while(!okInput); //loop until valid choice entered
+
+                if (playerAMove.equalsIgnoreCase("R")) { //Player A chose Rock
+                    if (playerBMove.equalsIgnoreCase("R")) {
+                        //Rock vs Rock
+                        ties++;
+                        System.out.println("Rock vs Rock, it's a Tie!");
+                    }
+                    else if (playerBMove.equalsIgnoreCase("P")) {
+                        //Rock vs Paper
+                        playerBWins++;
+                        System.out.println("Paper covers Rock, Player B Wins!");
+                    }
+                    else if (playerBMove.equalsIgnoreCase("S")) {
+                        //Rock vs Scissors
+                        playerAWins++;
+                        System.out.println("Rock breaks Scissors, Player A wins!");
+                    }
+                    else { //included for 'impossible' error catching
+                        System.out.println("Unexpected result. Check program code line 71");
+                    }
+                }
+                else if (playerAMove.equalsIgnoreCase("P")) { //Player A chose Paper
+                    if (playerBMove.equalsIgnoreCase("R")) {
+                        //Paper vs Rock
+                        playerAWins++;
+                        System.out.println("Paper covers Rock, Player A wins!");
+                    }
+                    else if (playerBMove.equalsIgnoreCase("P")) {
+                        //Paper vs Paper
+                        ties++;
+                        System.out.println("Paper vs Paper, it's a tie!");
+                    }
+                    else if (playerBMove.equalsIgnoreCase("S")) {
+                        //Paper vs Scissors
+                        playerBWins++;
+                        System.out.println("Paper vs Scissors, Player B wins!");
+                    }
+                    else { // included for 'impossible' error catching
+                        System.out.println("Unexpected result, check program line 91");
+                    }
+                }
+                else if (playerAMove.equalsIgnoreCase("S")) { //Player A chose Scissors
+                    if (playerBMove.equalsIgnoreCase("R")) {
+                        //Scissors vs Rock
+                        playerBWins++;
+                        System.out.println("Rock breaks Scissors, Player B wins!");
+                    }
+                    else if (playerBMove.equalsIgnoreCase("P")) {
+                        //Scissors vs Paper
+                        playerAWins++;
+                        System.out.println("Scissors cuts Paper, Player A wins!");
+                    }
+                    else if (playerBMove.equalsIgnoreCase("S")) {
+                        //Scissors vs Scissors
+                        ties++;
+                        System.out.println("Scissors vs Scissors, it's a tie!");
+                    }
+                    else { // included for 'impossible' error catching
+                        System.out.println("Unexpected result. Check program code line 111");
+                    }
+                }
+                else { //included for error catching
+                    System.out.println("Unexpected Player A input. Check program code line 115");
+                }
+
+                System.out.println("\nScore   PlayerA: " + playerAWins + "   PlayerB: " + playerBWins + "   Ties: " + ties);
 
                 System.out.print("\nContinue? [Y/N]: ");
                 continueYN = in.nextLine();
